97436dabf86e85321c3f977106ea9700d6f0ff1b
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2014-12-03 11:03:45 -0500

    Walk tree in reverse chrono to build .git-presentation file

--- cmd/init.go
+++ cmd/init.go
@@ -3,9 +3,12 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/codegangsta/cli"
+	"github.com/libgit2/git2go"
 )
 
 var CmdInit = cli.Command{
@@ -16,13 +19,91 @@ You can edit use this file to customize which commits will be included in the pr
 	Action: runInit,
 	Flags: []cli.Flag{
 		cli.BoolFlag{"verbose, v", "show process details", ""},
+		cli.StringFlag{"repo, r", ".", "present specified repository", ""},
 	},
+}
+
+const PRESENTATION_FILE = ".git-presentation"
+
+type PresentationConfig struct {
+	Repo    string       `json:"repository"`
+	Commits []ConfigMeta `json:"commits"`
+}
+
+type ConfigMeta struct {
+	Sha     string `json:"sha"`
+	Message string `json:"message"`
 }
 
 func runInit(ctx *cli.Context) {
 	if ctx.Bool("verbose") {
-		fmt.Println("Running in verbose mode")
+		fmt.Printf("Initializing %s file\n", PRESENTATION_FILE)
 	}
-	fmt.Println("TODO: run init process here...")
+
+	repoPath := ctx.String("repo")
+
+	repo, err := git.OpenRepository(repoPath)
+	if err != nil {
+		fmt.Println("Problem opening repository:", err)
+		return
+	}
+	defer repo.Free()
+
+	revWalker, err := repo.Walk()
+	if err != nil {
+		fmt.Println("Problem walking repository commit history:", err)
+		return
+	}
+	defer revWalker.Free()
+
+	err = revWalker.PushHead()
+	if err != nil {
+		fmt.Println("Problem pushing HEAD onto walker:", err)
+		return
+	}
+
+	revWalker.Sorting(git.SortReverse)
+
+	err = createPresentationFile(*revWalker, repo.Path(), ctx.Bool("verbose"))
+	if err != nil {
+		fmt.Printf("Error creating %s file: %q\n", PRESENTATION_FILE, err)
+		return
+	}
+	return
+}
+
+func createPresentationFile(walker git.RevWalk, repoPath string, verbose bool) (err error) {
+	f, err := os.Create(PRESENTATION_FILE)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			return
+		}
+	}()
+
+	config := new(PresentationConfig)
+	config.Repo = repoPath
+
+	iterator := func(commit *git.Commit) bool {
+		if verbose {
+			fmt.Printf("Adding Commit: %s => %s", commit.Id(), commit.Message())
+		}
+		config.Commits = append(config.Commits, ConfigMeta{commit.Id().String(), commit.Message()})
+		return true
+	}
+
+	err = walker.Iterate(iterator)
+	if err != nil {
+		return
+	}
+
+	configJSON, err := json.Marshal(config)
+	if err != nil {
+		return
+	}
+
+	_, err = f.Write(configJSON)
 	return
 }
